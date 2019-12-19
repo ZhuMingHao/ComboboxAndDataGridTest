89b1498780abc41635d06b8db960b913c31b4812
msdnwptest <54531691+msdnwptest@users.noreply.github.com>
2019-12-19 21:38:21 +0800

    Add project files.

---
+++ TestCore/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using GalaSoft.MvvmLight;
+
+namespace TestCore.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
